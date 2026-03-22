0b088f30e8732de093efd3d87792e5d17c22bca8
shripad.d007 <shripad.d007@gmail.com>
2026-03-22 11:25:39 +0530

    third

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from DevOps CI/CD Pipeline!\n');
+  res.end('Shreyansh Prince!\n');
 });
 
 server.listen(port, hostname, () => {
